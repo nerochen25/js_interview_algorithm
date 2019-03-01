73ab38742389073e2ad22dd708fb7aba89519fc8
Nero <nerochen25@berkeley.edu>
2019-02-28 18:49:17 -0800

    Add solution to primeFactors with O(n) time

--- js_interview_algorithm.js
+++ js_interview_algorithm.js
@@ -25,3 +25,32 @@ const isPrime = (n) => {
 }
 
 console.log(isPrime(1));
+
+
+// 2. Prime Factors
+// Question: How could you find all prime factors of a number?
+
+// Answer: Run a while loop. start dividing by two and if not divisible increase divider until u r done.
+
+const primeFactors = (n) => {
+    //divisible factors that are primes
+    var factors = [],
+        divisor = 2;
+
+    while (n >= 2) {
+        if (n % divisor === 0) {
+            factors.push(divisor);
+            n = n / divisor;
+        } else {
+            divisor++;
+        }
+    }
+
+    return factors;
+}
+
+// console.log(primeFactors(8));
+
+const fibonacci = (n) => {
+    
+}
