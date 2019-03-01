36503de9edfd4deaec9879e0e2e0444e17619095
Nero <nerochen25@berkeley.edu>
2019-02-28 18:58:08 -0800

    Add solution to fibonacci with O(n) time

--- js_interview_algorithm.js
+++ js_interview_algorithm.js
@@ -52,5 +52,17 @@ const primeFactors = (n) => {
 // console.log(primeFactors(8));
 
 const fibonacci = (n) => {
-    
+    var fibo = [0,1];
+
+    if (n <= 2) {
+        return 1;
+    }
+
+    for (let i = 2; i <= n; i++) {
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+    }
+
+    return fibo[n];
 }
+
+console.log(fibonacci(5))
