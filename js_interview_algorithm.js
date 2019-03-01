5087bc038e43bd30d1d834cd06b742f15f74da4b
Nero <nerochen25@berkeley.edu>
2019-02-28 19:19:52 -0800

    Add solution to greatestCommonDivisor with O(n) time

--- js_interview_algorithm.js
+++ js_interview_algorithm.js
@@ -87,4 +87,27 @@ const recursiveFibonacci = (n) => {
     }
 }
 
-console.log(recursiveFibonacci(5))
+// console.log(recursiveFibonacci(5))
+
+// 4. Greatest Common Divisor
+// Question: How would you find the greatest common divisor of two numbers?
+
+const greatestCommonDivisor = (a, b) => {
+    var divisor = 2,
+        greatestDivisor = 1;
+    
+    if (a < 2 || b < 2) {
+        return 1;
+    }
+
+    while (a >= divisor && b >= divisor) {
+        if (a % divisor === 0 && b % divisor === 0) {
+            greatestDivisor = divisor;
+        }
+        divisor++;
+    }
+
+    return greatestDivisor;
+}
+
+// console.log(greatestCommonDivisor(10,4));
