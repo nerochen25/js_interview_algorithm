4b8a38c9c9bdbe5554cf9afe0822a329ccc9aa21
Nero <nerochen25@berkeley.edu>
2019-02-28 18:38:28 -0800

    Add solution to isPrime with O(n) time

--- js_interview_algorithm.js
+++ js_interview_algorithm.js
@@ -1,4 +1,27 @@
-// 1. check Prime
+// 1. Check Prime
 // Question: How would you verify a prime number?
 
 // Answer: a prime number is only divisible by itself and 1. So, i will run a while loop and increase by 1. (look at the code example. If you dont get it. this is not your cake. do learn javaScript basics and come back.)
+
+const isPrime = (n) => {
+    var divisor = 2;
+
+    //1 is not prime;
+    if (n < 2) {
+        return false;
+    }
+
+    while (n > divisor) {
+        if (n % divisor === 0) {
+            return false;
+        } else if (divisor >= 3) {
+            divisor += 2;
+        } else {
+            divisor++;
+        }
+    }
+
+    return true;
+}
+
+console.log(isPrime(1));
