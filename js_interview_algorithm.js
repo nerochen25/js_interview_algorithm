e6aa899929d5878c6d6ea1dc0fd6ed5a7edd1fbe
Nero <nerochen25@berkeley.edu>
2019-03-05 19:38:06 -0800

    Add     let maxNum = Math.max(...arr);

--- js_interview_algorithm.js
+++ js_interview_algorithm.js
@@ -313,4 +313,28 @@ const rand7 = () => {
     return 5 + rand5() / 5 * 2
 }
 
-console.log(rand7());
+// console.log(rand7());
+
+// 15. missing number
+// Question: from a unsorted array of numbers 1 to 100 excluding one number, how will you find that number.
+
+// Explanation: You have an array of numbers 1 to 100 in an array. Only one number is missing in the array. The array is unsorted. Find the missing number.
+
+// Answer: You have to act like you are thinking a lot. and then talk about the sum of a linear series of n numbers = n*(n+1)/2
+
+const missingNum = arr => {
+    let maxNum = Math.max(...arr);
+    let minNum = Math.min(...arr);
+    let missingNumArr = []
+
+    for (let i = minNum; i <= maxNum; i++) {
+        if (!arr.includes(i)) {
+            missingNumArr.push(i)
+        }
+    }
+
+    return missingNumArr;
+
+}
+
+console.log(missingNum([1,4,3,6,7,14]));
