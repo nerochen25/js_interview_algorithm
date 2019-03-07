7298f98b44807a9c6c3f3b8f57aaad6111c1fb72
Nero <nerochen25@berkeley.edu>
2019-03-06 22:43:58 -0800

    Add solution to countZero

--- js_interview_algorithm.js
+++ js_interview_algorithm.js
@@ -396,3 +396,14 @@ function topSum(arr){
 
 // Explanation: So the tick here is. if you have a number 1 to 50 the value is 5. just 50 divided by 10. However, if the value is 100. the value is 11. you will get by 100/10 = 10 and 10/10. Thats how you will get in the more zeros in one number like (100, 200, 1000)
 
+function countZero(n){
+    var count = 0;
+    while(n>0){
+     count += Math.floor(n/10);
+     n = n/10;
+    }
+    return count;
+  }
+  
+//   > countZero(2014);
+//     = 223
