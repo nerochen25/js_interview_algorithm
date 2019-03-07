08222ad2f9bfc05a39e6914335e1eb125d5f6a4a
Nero <nerochen25@berkeley.edu>
2019-03-06 22:44:31 -0800

    Add subString function

--- js_interview_algorithm.js
+++ js_interview_algorithm.js
@@ -407,3 +407,9 @@ function countZero(n){
   
 //   > countZero(2014);
 //     = 223
+
+// 19. subString
+// Question: How can you match substring of a sting?
+
+// Answer: Will use to pointer (one for string and another for the substring) while iterating the string. And will have another variable to hold the starting index of the initial match.
+
