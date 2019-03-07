5cf4c66c9a315c18bb5f7a5126580fb905eef30f
Nero <nerochen25@berkeley.edu>
2019-03-06 22:45:47 -0800

    Add permutation function and solution

--- js_interview_algorithm.js
+++ js_interview_algorithm.js
@@ -437,3 +437,35 @@ function subStringFinder(str, subStr){
     return -1;
   }
   
+//   20. Permutations
+//   Question: How would you create all permutation of a string?
+  
+//   Answer: This could be a tough one based on you level of knowledge about algorithm.
+
+function permutations(str){
+    var arr = str.split(''),
+        len = arr.length, 
+        perms = [],
+        rest,
+        picked,
+        restPerms,
+        next;
+    
+        if (len == 0)
+            return [str];
+    
+        for (var i=0; i<len; i++)
+        {
+            rest = Object.create(arr);
+            picked = rest.splice(i, 1);
+    
+            restPerms = permutations(rest.join(''));
+    
+           for (var j=0, jLen = restPerms.length; j< jLen; j++)
+           {
+               next = picked.concat(restPerms[j]);
+               perms.push(next.join(''));
+           }
+        }
+       return perms;
+    }
