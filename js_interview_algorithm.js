d44e56972ba850942fb5739d36b55713e0936536
Nero <nerochen25@berkeley.edu>
2019-03-06 22:45:02 -0800

    Add solution to subString

--- js_interview_algorithm.js
+++ js_interview_algorithm.js
@@ -413,3 +413,27 @@ function countZero(n){
 
 // Answer: Will use to pointer (one for string and another for the substring) while iterating the string. And will have another variable to hold the starting index of the initial match.
 
+function subStringFinder(str, subStr){
+    var idx = 0,
+        i = 0,
+        j = 0,
+        len = str.length,
+        subLen = subStr.length;
+  
+     for(; i<len; i++){
+     
+        if(str[i] == subStr[j])
+           j++;
+        else
+           j = 0;
+        
+        //check starting point or a match   
+        if(j == 0)
+          idx = i;
+        else if (j == subLen)
+          return idx;
+    }
+  
+    return -1;
+  }
+  
