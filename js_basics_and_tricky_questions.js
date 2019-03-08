a36f6e605836d096548e2373f696e8b976c87ea8
Nero <nerochen25@berkeley.edu>
2019-03-07 19:33:31 -0800

    Add solution to Object Equality

--- js_basics_and_tricky_questions.js
+++ js_basics_and_tricky_questions.js
@@ -1,2 +1,27 @@
 // 3. Object Equality
 // How would you compare two objects in JS?
+// First to check the lengths of both objects
+// Second to check the values of the same keys are the same;
+// Orders of the keys don't matter
+function isEqual(a, b) {
+    let aKeys = Object.keys(a);
+    let bKeys = Object.keys(b);
+
+    if (aKeys.length !== bKeys.length) {
+        return false;
+    }
+
+    for (let i = 0; i < aKeys.length; i++) {
+        var currentKey = aKeys[i];
+
+        if (a[currentKey] !== b[currentKey]) {
+            return false;
+        }  
+    }
+
+    return true;
+}
+
+var a = {b: 3, a: 1};
+var b = {a: 1, b: 3};
+console.log(isEqual(a,b));
